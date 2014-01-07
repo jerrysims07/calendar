765ab49d4be3b310e5aeca99c679279e3f165ed6
Jerry Sims <jerrysims07@gmail.com>
2014-01-07 09:11:36 -0600

    test

--- cal.rb
+++ cal.rb
@@ -1,6 +1,13 @@
+require_relative "lib/helper"
+
 if(ARGV.length == 2)
 	month = ARGV[0]
 	year = ARGV[1]
+	process_one_month(month, year)
 else
 	year = ARGV[0]
+	process_full_year(year)
 end
+
+puts "hello, world"
+
---
+++ lib/helper.rb
@@ -0,0 +1,10 @@
+require_relative "./month"
+require_relative "./year"
+require_relative "./zeller"
+
+def process_one_month(month, year)
+	
+end
+
+def process_full_year(year)
+end
